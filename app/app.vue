8f61d01e3a2ebfc5bb643929a7d2d0f00e0a35f1
Clemens Sietas <siecle@gmx.de>
2026-06-15 17:51:37 +0200

    Default UI in layouts and index.vue in pages

--- app/app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Nuxt Minimal Starter</h1>
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['@picocss/pico'],
   modules: ['@nuxt/eslint-config'],
 });
