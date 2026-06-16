6c251cceaee859bcb32162b7a4ae6a245306b3a9
Clemens Sietas <siecle@gmx.de>
2026-06-16 22:48:52 +0200

    Add more tasks

--- server/api/tasks.get.ts
+++ server/api/tasks.get.ts
@@ -2,7 +2,10 @@ export default defineEventHandler(async (event) => {
   const tasks = [
     { id: 1, title: 'Learn Nuxt', completed: false },
     { id: 2, title: 'Learn Vue', completed: true },
-    { id: 3, title: 'Learn TypeScript', completed: true },
+    { id: 3, title: 'Learn PHP', completed: true },
+    { id: 4, title: 'Learn Symfony', completed: true },
+    { id: 5, title: 'Learn TypeScript', completed: true },
+    { id: 6, title: 'Learn React', completed: true },
   ];
 
   return tasks;
